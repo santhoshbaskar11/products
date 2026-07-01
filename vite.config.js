7ed217c0b59ac5bf74f8e384034f717b8c33eca4
santhoshbaskar11 <santhoshbaskar3110@gmail.com>
2026-07-01 22:14:45 +0530

    Use relative base path (./) to support Vercel root deployments and GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/products/',
+  base: './',
   plugins: [
     react(),
     tailwindcss(),
